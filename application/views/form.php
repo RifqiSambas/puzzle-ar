ff18c9c7eb12c71617abfa115927ee450229f19b
rifqisambas <rifqisambas@tuta.io>
2020-06-27 15:45:24 +0700

    cleaning code

--- application/views/form.php
+++ application/views/form.php
@@ -6,12 +6,13 @@
 					<h1>Puzzle AR</h1>
 				</div>
 				<div class="card-body">
-					<h5>situs ini adalah sebuah permainan berbasis web</h5>
+					<h5>situs ini adalah sebuah permainan puzzle berbasis web</h5>
 					<p>Silahkan ambil gambar untuk dijadikan puzzle</p>
+					<p><a href="">what's this? how to play? f.a.q, etc</a></p>
 					<div id="my_camera"></div>
 					<br>
 					<?= form_open_multipart('site/upload') ?>
-					<input class="btn btn-primary" type=button value="Take Large Snapshot" onClick="take_snapshot()">
+					<input class="btn btn-primary" type=button value="Take Snapshot" onClick="take_snapshot()">
 				</div>
 			</div>
 		</div>
@@ -25,36 +26,22 @@
 </section>
 
 
-<!-- First, include the Webcam.js JavaScript Library -->
 <script src="<?= base_url('assets/js/webcam.min.js') ?>"></script>
-<!-- Configure a few settings and attach camera -->
-<script language="JavaScript">
+<script>
 	Webcam.set({
-		// live preview size
 		width: 320,
 		height: 240,
-
-		// device capture size
 		dest_width: 640,
 		dest_height: 480,
-
-		// final cropped size
 		crop_width: 480,
 		crop_height: 480,
-
-		// format and quality
-		image_format: 'jpeg',
-		jpeg_quality: 90
+		image_format: 'png',
 	});
 
 	Webcam.attach('#my_camera');
-</script>
-<!-- Code to handle taking the snapshot and displaying it locally -->
-<script language="JavaScript">
+
 	function take_snapshot() {
-		// take snapshot and get image data
 		Webcam.snap(function(data_uri) {
-			// display results in page
 			document.getElementById('image-tag').value = data_uri;
 			document.getElementById('results').innerHTML =
 				'<h2>Here is your large image:</h2>' +
