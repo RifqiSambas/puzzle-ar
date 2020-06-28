16007f7aa9a6348796c0f65d6a0eede435e5c452
rifqisambas <rifqisambas@tuta.io>
2020-06-28 12:18:21 +0700

    update

--- application/views/client/index.php
+++ application/views/client/index.php
@@ -15,16 +15,46 @@
 			width: 460px;
 		}
 	</style>
+	<script>
+		window.onload = function() {
+			// find the element that you want to drag.
+			var box = document.getElementById('box');
+
+			/* listen to the touchMove event,
+			every time it fires, grab the location
+			of touch and assign it to box */
+
+			box.addEventListener('touchmove', function(e) {
+				// grab the location of touch
+				var touchLocation = e.targetTouches[0];
+
+				// assign box new coordinates based on the touch.
+				box.style.left = touchLocation.pageX + 'px';
+				box.style.top = touchLocation.pageY + 'px';
+			})
+
+			/* record the position of the touch
+			when released using touchend event.
+			This will be the drop position. */
+
+			box.addEventListener('touchend', function(e) {
+				// current box position.
+				var x = parseInt(box.style.left);
+				var y = parseInt(box.style.top);
+			})
+
+		}
+	</script>
 	<div id="puzzle">
-		<div class="box"><img alt="0"></div>
-		<div class="box"><img alt="1"></div>
-		<div class="box"><img alt="2"></div>
-		<div class="box"><img alt="3"></div>
-		<div class="box"><img alt="4"></div>
-		<div class="box"><img alt="5"></div>
-		<div class="box"><img alt="6"></div>
-		<div class="box"><img alt="7"></div>
-		<div class="box"><img alt="8"></div>
+		<div id="box" class="box"><img alt="0"></div>
+		<div id="box" class="box"><img alt="1"></div>
+		<div id="box" class="box"><img alt="2"></div>
+		<div id="box" class="box"><img alt="3"></div>
+		<div id="box" class="box"><img alt="4"></div>
+		<div id="box" class="box"><img alt="5"></div>
+		<div id="box" class="box"><img alt="6"></div>
+		<div id="box" class="box"><img alt="7"></div>
+		<div id="box" class="box"><img alt="8"></div>
 	</div>
 	<script>
 		var image = document.getElementsByTagName("img");
--- application/views/puzzle/index.php
+++ application/views/puzzle/index.php
@@ -1,12 +1,9 @@
 <a-scene embedded artoolkit='sourceType: webcam; detectionMode: mono_and_matrix; matrixCodeType: 3x3;'>
-
-	<form action="" method="post">
-		<?php for ($i = 0; $i < 9; $i++) { ?>
-			<a-marker type="pattern" url="<?= base_url('assets/marker/pattern-0') . $i . '.patt' ?>">
-				<a-plane id="logo" src="<?= base_url('assets/uploads/') . $i . '.png' ?>" position="0 0.5 0" rotation="-90 0 0" width="2.0" height="2.2">
-				</a-plane>
-			</a-marker>
-		<?php } ?>
-		<a-entity camera></a-entity>
-	</form>
+	<?php for ($i = 0; $i < 9; $i++) { ?>
+		<a-marker type="pattern" url="<?= base_url('assets/marker/pattern-0') . $i . '.patt' ?>">
+			<a-plane id="logo" src="<?= base_url('assets/uploads/') . $i . '.png' ?>" position="0 0.5 0" rotation="-90 0 0" width="2.0" height="2.2">
+			</a-plane>
+		</a-marker>
+	<?php } ?>
+	<a-entity camera></a-entity>
 </a-scene>
